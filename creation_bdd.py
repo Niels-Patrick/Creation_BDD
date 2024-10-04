7123a01a4457e0dac94a1b6dab4f7b367b93f829
Niels-Patrick TESTON <niels.teston@gmail.com>
2024-10-04 15:34:34 +0200

    Update creation_bdd.py, importation_bdd.py (try...except: rollback)

--- creation_bdd.py
+++ creation_bdd.py
@@ -5,15 +5,18 @@ con = sqlite3.connect("brief_cbdd.db")
 # Creating the cursor
 cur = con.cursor()
 
+try:
 # Opening and reading the SQL file
-with open("Brief_CBDD.sql", "r") as sql_file:
-    content = sql_file.read()
+    with open("Brief_CBDD.sql", "r") as sql_file:
+        content = sql_file.read()
 
-# Spliting the SQL queries
-content_list = content.split(";", 1)
-# Executing the content of the SQL file, creating the database's tables
-for query in content_list:
-    cur.execute(query)
-
-# Closing the connection
-con.close()
+    # Spliting the SQL queries
+    content_list = content.split(";", 1)
+    # Executing the content of the SQL file, creating the database's tables
+    for query in content_list:
+        cur.execute(query)
+except:
+    con.rollback()
+finally:
+    # Closing the connection
+    con.close()
--- importation_bdd.py
+++ importation_bdd.py
@@ -6,31 +6,37 @@ con = sqlite3.connect("brief_cbdd.db")
 # Creating the cursor
 cur = con.cursor()
 
-# Opening and reading the "clients" CSV file
-with open("jeu-de-donnees-clients-66fed38c68779376654152.csv", "r") as csv_clients:
-    csv_clients_reader = csv.reader(csv_clients)
-    next(csv_clients_reader)
+try:
+    # Opening and reading the "clients" CSV file
+    with open("jeu-de-donnees-clients-66fed38c68779376654152.csv", "r") as csv_clients:
+        csv_clients_reader = csv.reader(csv_clients)
+        next(csv_clients_reader)
 
-    # Inserting the "clients" CSV data into the database
-    for row in csv_clients_reader:
-        cur.execute("""
-                    INSERT INTO Client (client_id, nom, prenom, email, telephone, date_naissance, adresse, consentement_marketing)
-                    VALUES (?,?,?,?,?,?,?,?);
-        """, row)
-con.commit()
+        # Inserting the "clients" CSV data into the database
+        for row in csv_clients_reader:
+            cur.execute("""
+                        INSERT INTO Client (client_id, nom, prenom, email, telephone, date_naissance, adresse, consentement_marketing)
+                        VALUES (?,?,?,?,?,?,?,?);
+            """, row)
+    con.commit()
+except:
+    con.rollback()
 
-# Opening and reading the "commandes" CSV file
-with open("jeu-de-donnees-commandes-66fe65226fdb5678959707.csv", "r") as csv_commandes:
-    csv_commandes_reader = csv.reader(csv_commandes)
-    next(csv_commandes_reader)
+try:
+    # Opening and reading the "commandes" CSV file
+    with open("jeu-de-donnees-commandes-66fe65226fdb5678959707.csv", "r") as csv_commandes:
+        csv_commandes_reader = csv.reader(csv_commandes)
+        next(csv_commandes_reader)
 
-    # Inserting the "commandes" CSV data into the database
-    for row in csv_commandes_reader:
-        cur.execute("""
-                    INSERT INTO Commande (commande_id, client_id, date_commande, montant_commande)
-                    VALUES (?,?,?,?);
-        """, row)
-con.commit()
-
-# Closing the connection
-con.close()
+        # Inserting the "commandes" CSV data into the database
+        for row in csv_commandes_reader:
+            cur.execute("""
+                        INSERT INTO Commande (commande_id, client_id, date_commande, montant_commande)
+                        VALUES (?,?,?,?);
+            """, row)
+    con.commit()
+except:
+    con.rollback()
+finally:
+    # Closing the connection
+    con.close()
