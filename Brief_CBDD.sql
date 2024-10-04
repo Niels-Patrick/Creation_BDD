1f6f90b2bd74a7009e115d2cf08184c2e34e7878
Niels-Patrick TESTON <niels.teston@gmail.com>
2024-10-04 12:16:42 +0200

    Initial commit

---
+++ Brief_CBDD.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS "Client" (
+	"client_id" INTEGER NOT NULL UNIQUE,
+	"nom" VARCHAR NOT NULL,
+	"prenom" VARCHAR NOT NULL,
+	"email" VARCHAR NOT NULL UNIQUE,
+	"telephone" VARCHAR,
+	"date_naissance" DATE,
+	"adresse" VARCHAR,
+	"consentement_marketing" BOOLEAN NOT NULL,
+	PRIMARY KEY("client_id")
+);
+
+CREATE TABLE IF NOT EXISTS "Commande" (
+	"commande_id" INTEGER NOT NULL UNIQUE,
+	"date_commande" DATE NOT NULL,
+	"montant_commande" REAL NOT NULL,
+	"client_id" INTEGER NOT NULL,
+	PRIMARY KEY("commande_id"),
+	FOREIGN KEY ("client_id") REFERENCES "Client"("client_id")
+	ON UPDATE NO ACTION ON DELETE NO ACTION
+);
---
+++ README.md
@@ -0,0 +1,11 @@
+# Brief Creation BDD
+
+This is a python script to create a database and its tables, based on an SQL file genereted from a MCD/MLD/MPD tool [(drawdb.app)](https://www.drawdb.app/editor).
+
+## Usage
+
+Open the creation_bdd.py file on a Python IDE (PyCharm, VSCode) and run it. The database will be created as well as its tables.
+
+## Result
+
+![alt text]()
